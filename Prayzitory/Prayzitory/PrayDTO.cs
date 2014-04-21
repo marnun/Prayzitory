e9dd1098f5afbd8fd33c992707fbe126aeb70528
Erez Lefel <erezl@rumble.me>
2014-04-21 15:12:19 +0300

    first commit

---
+++ Prayzitory/Prayzitory/PrayDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prayzitory
+{
+    public class PrayDTO
+    {
+        private string _pray;
+        private string _user;
+        private DateTime _timestemp;
+
+        public PrayDTO(string pray, string user, DateTime timestemp)
+        {
+            _pray = pray;
+            _user = user;
+            _timestemp = timestemp;
+        }
+
+        public string Pray
+        {
+            get { return _pray; }
+        }
+
+        public string User
+        {
+            get { return _user; }
+        }
+
+        public DateTime Timestemp
+        {
+            get { return _timestemp; }
+        }
+    }
+}
