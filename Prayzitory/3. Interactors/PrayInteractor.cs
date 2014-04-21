9ae55a5cb4ee0e98e0b3496602620b9da5094e61
giladw <giladw@outlook.com>
2014-04-21 17:58:16 +0300

    Projects By Layers

---
+++ Prayzitory/3. Interactors/PrayInteractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interactors
+{
+    public class PrayInteractor
+    {
+        public void CreateNewPray(string pray, DateTime timestemp, string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PrayDto> GetTopPrays()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Prayzitory/4. Presenters/Controllers/PraysController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Interactors;
+
+namespace Presenters.Controllers
+{
+    public interface IPraysController
+    {
+        void NewPray(string text, DateTime timestemp, string user);
+        List<PrayDto> ListPrays();
+    }
+
+    public class PraysController : IPraysController
+    {
+        public void NewPray(string text, DateTime timestemp, string user)
+        {
+            //calls add new text UseCase
+            var interactor = new PrayInteractor();
+            interactor.CreateNewPray(text, timestemp, user);
+        }
+
+        public List<PrayDto> ListPrays()
+        {
+            var interactor = new PrayInteractor();
+            List<PrayDto> result = interactor.GetTopPrays();
+
+            return result;
+        }
+    }
+}
