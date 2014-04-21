a8483575d336c46cfcdc6a055e1b4094007b0da4
giladw <giladw@outlook.com>
2014-04-21 15:58:44 +0300

    create paryer controller

--- Prayzitory/Prayzitory/IPrayzitoryController.cs
+++ Prayzitory/Prayzitory/IPrayzitoryController.cs
@@ -10,4 +10,22 @@ namespace Prayzitory
         void NewPray(PrayDTO prayDto);
         List<PrayDTO> ListPrays();
     }
+
+    public class Prayzitory : IPrayzitoryController
+    {
+        public void NewPray(PrayDTO prayDto)
+        {
+            //calls add new pray UseCase
+            var interactor = new PrayInteractor();
+            interactor.CreateNewPray(prayDto.Pray, prayDto.Timestemp, prayDto.User);
+        }
+
+        public List<PrayDTO> ListPrays()
+        {
+            var interactor = new PrayInteractor();
+            List<PrayDTO> result = interactor.GetTopPrays();
+            
+            return result;
+        }
+    }
 }
